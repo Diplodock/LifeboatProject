69898ac6c1a01859b8c654f1b406aa515f0d5361
Вот так <vottak@myhost>
2020-03-14 11:15:47 +0300

    Fixes, structures and dynamic_cast

--- src/logic/Card.h
+++ src/logic/Card.h
@@ -3,18 +3,24 @@
 
 #include <string>
 
+struct CardOptions {
+    std::string name;
+    std::string description;
+    int owner;
+};
+
 class Card {
   public:
     std::string GetName() const;
     std::string GetDescription() const;
     int GetOwner() const;
+    
     void SetName(std::string name);
     void SetDescription(std::string description);
     void SetOwner(int owner);
+
   protected:
-    std::string name_;
-    std::string description_;
-    int owner_;
+    CardOptions cardOptions_;
 };
 
 #endif // LIFEBOAT_LOGIC_CARD_H_
--- src/logic/Weapon.h
+++ src/logic/Weapon.h
@@ -7,6 +7,7 @@ class Weapon : public Item {
   public:
     Weapon(int damage_points);
     int GetDamagePoints();
+    
   private:
     int damage_points_;
 };
