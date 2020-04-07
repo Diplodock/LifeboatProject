7170fe406a1406cc63b19374de12a077f4d651ff
Ekaterina Shelukhina <shelukchina2447@gmail.com>
2020-04-07 13:49:44 +0500

    add lifeboat

--- src/multiplayer/lifeboat/player.h
+++ src/multiplayer/lifeboat/player.h
@@ -1,16 +1,92 @@
-//
-// Created by Екатерина Шелухина on 07.04.2020.
-//
-
 #ifndef LIFEBOAT_PLAYER_H
 #define LIFEBOAT_PLAYER_H
 
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <grpc++/grpc++.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/security/credentials.h>
+
+#include "lifeboat.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::ClientReader;
+using grpc::ClientReaderWriter;
+using grpc::ClientWriter;
+using grpc::Status;
+using grpc::WriteOptions;
+using lifeboat::Reply;
+using lifeboat::Request;
+using lifeboat::Game;
 
 
-class player {
+std::string grpc_code_to_str(grpc::StatusCode code) {
+    using sc = grpc::StatusCode;
 
+    switch (code) {
+        case sc::OK:
+            return "OK";
+        case sc::CANCELLED:
+            return "CANCELLED";
+        case sc::INVALID_ARGUMENT:
+            return "INVALID_ARGUMENT";
+        case sc::DEADLINE_EXCEEDED:
+            return "DEADLINE_EXCEEDED";
+        case sc::NOT_FOUND:
+            return "NOT_FOUND";
+        case sc::ALREADY_EXISTS:
+            return "ALREADY_EXISTS";
+        case sc::PERMISSION_DENIED:
+            return "PERMISSION_DENIED";
+        case sc::UNAUTHENTICATED:
+            return "UNAUTHENTICATED";
+        case sc::RESOURCE_EXHAUSTED:
+            return "RESOURCE_EXHAUSTED";
+        case sc::FAILED_PRECONDITION:
+            return "FAILED_PRECONDITION";
+        case sc::ABORTED:
+            return "ABORTED";
+        case sc::OUT_OF_RANGE:
+            return "OUT_OF_RANGE";
+        case sc::UNIMPLEMENTED:
+            return "UNIMPLEMENTED";
+        case sc::INTERNAL:
+            return "INTERNAL";
+        case sc::UNAVAILABLE:
+            return "UNAVAILABLE";
+        case sc::DATA_LOSS:
+            return "DATA_LOSS";
+        case sc::DO_NOT_USE:
+            return "DO_NOT_USE";
+        case sc::UNKNOWN:
+            return "UNKNOWN";
+    }
+    return "UNKNOWN";
+}
+
+std::string grpc_status_to_str(const grpc::Status &status) {
+    grpc::StatusCode err_code = status.error_code();
+    std::string err_message = status.error_message();
+    std::string err_details = status.error_details();
+
+    return std::string("StatusCode: (") + std::to_string(err_code) + ") " + grpc_code_to_str(err_code) + "\n"
+           + "Message: " + err_message + "\n"
+           + "Details: " + err_details + "\n";
+}
+
+class Player {
+public:
+    explicit Player(std::shared_ptr<Channel> channel) : stub_(
+            lifeboat::Game::NewStub(channel)) {}
+    void run_client(std::string user_name);
+
+private:
+    std::unique_ptr<Game::Stub> stub_;
 };
-
-
 
 #endif //LIFEBOAT_PLAYER_H
---
+++ src/multiplayer/lifeboat/server.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <grpc++/grpc++.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/security/credentials.h>
+
+#include "lifeboat.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
+using grpc::WriteOptions;
+using lifeboat::Reply;
+using lifeboat::Request;
+using lifeboat::Game;
+
+class GameImpl final : public Game::Service {
+    Status Connect(ServerContext *context, ServerReaderWriter <Reply, Request> *stream) override {
+        Request r;
+        while (stream->Read(&r)) {
+            std::cout << "registrate user" << r.user_name();
+            Reply reply;
+            WriteOptions writeOptions;
+            bool res = stream->Write(reply, writeOptions);
+        }
+        return Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string server_address("0.0.0.0:50051");
+    GameImpl service;
+    ServerBuilder builder;
+
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << '\n';
+    server->Wait();
+}
+
+int main() {
+    RunServer();
+    return 0;
+}
